1022199cca58941fe87d089ef92ecbf5e66f3c26
Vinicius <viniciussimm@alunos.utfpr.edu.br>
2025-07-06 22:54:30 -0300

    :rocket: update endpoint

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Query
 from app.ocr import extract_text_from_upload_file
 from app.llm import classify_text
 
 app = FastAPI()
 
 @app.post("/extract_text/")
-async def extract_text(file: UploadFile = File(...)):
+async def extract_text(
+    file: UploadFile = File(...),
+    apply_preprocessing: bool = Query(False, description="Apply image preprocessing?")
+):
     content = await file.read()
-    text = extract_text_from_upload_file(content, file.content_type)
+    text = extract_text_from_upload_file(content, file.content_type, apply_preprocessing)
 
     classification = classify_text(text)
 
-    return {"class": classification, "text": text}
+    return classification
 
